1cc4aa37c6c1e55043efd06ae57157bb4b6fc897
zelr0x <zelr0xdev@gmail.com>
2022-05-03 04:21:51 +0400

    Add 3.8

---
+++ ch3/examples/e3_8.rs
@@ -0,0 +1,21 @@
+use ch3::{AES256, hex2b};
+
+fn main() {
+    let k = hex2b(
+            "
+            80 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+            00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01
+            ",
+        )
+        .unwrap()
+        .try_into()
+        .unwrap();
+    let mut m: [u8; 16] = hex2b("53 9B 33 3B 39 70 6D 14 90 28 CF E1 D9 D4 A4 07")
+        .unwrap()
+        .try_into()
+        .unwrap();
+    let c = AES256::new(&k);
+    c.dec_mut(&mut m);
+    println!("{}", hex::encode(&m));
+}
+
