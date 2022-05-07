f54426a93e6b2a96b725ef05142c888f015a5cc9
zelr0x <zelr0xdev@gmail.com>
2022-05-07 19:21:05 +0400

    Change sha512n to return Vec<u8> for ergonomics, old is renamed to sha512n_const

--- src/sha.rs
+++ src/sha.rs
@@ -17,50 +17,54 @@ pub fn sha512(bytes: &[u8]) -> [u8; 64] {
 /// Panics:
 ///   if N <= 0
 ///   if N > 64
-pub fn sha512n<const N: usize>(bytes: &[u8]) -> [u8; N] {
+pub fn sha512n_const<const N: usize>(bytes: &[u8]) -> [u8; N] {
     assert!(N > 0);
     assert!(N <= 64);
 
     sha512(bytes)[..N].try_into().unwrap()
 }
 
+pub fn sha512n(bytes: &[u8], n: usize) -> Vec<u8> {
+    sha512(bytes)[..n].try_into().unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
-    fn sha512n_1_works() {
+    fn sha512n_const_1_works() {
         let expected = &sha512(b"hello sha512n")[..1];
-        let got = sha512n::<1>(b"hello sha512n");
+        let got = sha512n_const::<1>(b"hello sha512n");
         assert_eq!(got.len(), 1);
         assert_eq!(expected, got);
     }
 
     #[test]
-    fn sha512n_2_works() {
+    fn sha512n_const_2_works() {
         let expected = &sha512(b"hello sha512n")[..2];
-        let got = sha512n::<2>(b"hello sha512n");
+        let got = sha512n_const::<2>(b"hello sha512n");
         assert_eq!(got.len(), 2);
         assert_eq!(expected, got);
     }
 
     #[test]
-    fn sha512n_64_works() {
+    fn sha512n_const_64_works() {
         let expected = &sha512(b"hello sha512n")[..];
-        let got = sha512n::<64>(b"hello sha512n");
+        let got = sha512n_const::<64>(b"hello sha512n");
         assert_eq!(got.len(), 64);
         assert_eq!(expected, got);
     }
 
     #[test]
     #[should_panic]
-    fn sha512n_0_panics() {
-        sha512n::<0>(b"hello sha512n");
+    fn sha512n_const_0_panics() {
+        sha512n_const::<0>(b"hello sha512n");
     }
 
     #[test]
     #[should_panic]
-    fn sha512n_65_panics() {
-        sha512n::<65>(b"hello sha512n");
+    fn sha512n_const_65_panics() {
+        sha512n_const::<65>(b"hello sha512n");
     }
 }
