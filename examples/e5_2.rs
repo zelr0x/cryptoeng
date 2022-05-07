6fb7a207faa313c0d76ff879c26ab1a84658263f
zelr0x <zelr0xdev@gmail.com>
2022-05-07 19:21:57 +0400

    Cargo fmt

--- examples/e5_2.rs
+++ examples/e5_2.rs
@@ -1,7 +1,6 @@
-use cryptoeng::{sha, hex2b};
+use cryptoeng::{hex2b, sha};
 
 fn main() {
     let res = sha::sha256(&hex2b("48 65 6C 6C 6F 2C 20 77 6F 72 6C 64 2E 20 20 20").unwrap());
     println!("{}", hex::encode(&res));
 }
-
